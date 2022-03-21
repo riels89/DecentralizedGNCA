825e2654d1ed42423cea417218531a2d65045193
Riley White <white3rd@dukes.jmu.edu>
2022-03-21 16:49:29 -0400

    Made dgnca folder and setup.py file for this dir.

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='DecentralizedGNCA',
+      version='1.0',
+      description='Decentralized GNCA',
+      author='Riley White, Jonathan Lynch, Max Aedo Espicato, Maxwell Stevens',
+      author_email='rileywhite89@gmail.com',
+      url='https://github.com/riels89/DecentralizedGNCA',
+      packages=['gnca', "dgnca"],
+     )
