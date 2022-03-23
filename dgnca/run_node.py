423ae4e7b968826d0297ffdda2d9b54c9937ac44
Riley White <white3rd@dukes.jmu.edu>
2022-03-23 15:50:31 -0400

    Add pseudo code for our main script

---
+++ dgnca/run_node.py
@@ -0,0 +1,24 @@
+rank = 0
+
+
+if __name__ == "__main__":
+
+    #### Setup ####
+
+    # load model
+    # load data
+    # use rank to choose data to use
+    # Connect to main node
+
+    ### Main loop ###
+    # while no stop signal
+    #   Compute update
+    #   Check for stop signal
+
+    #   Broadcast update using tcp
+    #   Check for stop signal
+
+    #   Retrieve update from tcp buffer
+    #   Update data buffer
+    pass
+    
