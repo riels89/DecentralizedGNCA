d9f40c9ad0e272c2bfb88832848d230aeee44843
MaxRStevens-1 <steve3mr@dukes.jmu.edu>
2022-03-31 11:46:20 -0400

    finished parallel socket based mp

--- dgnca/node.py
+++ dgnca/node.py
@@ -4,23 +4,65 @@ import math
 from collections import namedtuple
 
 class Node:
-    def __init__(self, server, rank, port, num_proc):
-        self.rank = rank
+    def __init__(self, server, id, port, num_proc, lock):
+        self.id = id
         self.server = server
         self.port = port
         self.num_proc = num_proc
-        # hashtable to get socket with rank as key
+        self.messanger = -1
+        self.has_message = False
+        self.message = ""
+        self.message_ports = []
+        self.lock = lock
+        # hashtable to get socket with id as key
         self.communicators = {}
 
-    Communicator = namedtuple ("socket", "rank")
-    def addCommunicator (self, communicator, rank):
-        self.communicators[rank] = communicator
+    # largely depreciated
+    # ***
+    Communicator = namedtuple ("socket", "id")
+    def addCommunicator (self, communicator, id):
+        self.communicators[id] = communicator
+    # ***
     
+    # sends message to single port
+    # spins up new socket every time, not sure if this what do
+    def sendMessage (self, message, port):
+        self.messanger = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.messanger.connect(('localhost', port))
+        sock.sendall(bytes(message, 'ascii'))
+        
+    # checks if message flag has been raised
+    def hasMessage (self):
+        return self.has_message
+        
+    # cleans up node from sending messages
+    def finishedMessages (self):
+        self.message = ""
+        self.has_message = False
+    
+
+    # set ports for message to receive
+    def setMessagePorts (self, ports):
+        message_ports = ports
+        
+    # input message should use <string>.encode()
+    def setMessage (self, message):
+        self.message = message
+        self.has_message = True
+        # getters
+    def getMessagePorts (self):
+        return self.message_ports
+    def getServer (self):
+        return self.server
+    def getCommunicator (self, id):
+        return self.communicators[id]
+    def getCommunicators (self):
+        return self.communicators
+    def getLock(self):
+        return self.lock()
     def getPort (self):
         return self.port
-    
-    def getCommunicator (rank):
-        return communicators[rank]
-    def getCommunicators ():
-        return self.communicators
-    
+    def getMessage (self):
+        return self.message
+    def getId (self):
+        return self.id
--- dgnca/run_dgnca.py
+++ dgnca/run_dgnca.py
@@ -5,22 +5,17 @@ import math
 import sys
 from node import Node
 
-#shameless ripped from python docs
-#class ThreadedTCPRequestHandler(socketserver.BaseRequestHandler):
+num_procs = -1
+host = 'localhost'
+port_seed = 2000
+servers = []
+lock = threading.Lock()
+sum = 0
 
-#    def handle(self):
-#       data = str(self.request.recv(1024), 'ascii')
-#        cur_thread = threading.current_thread()
-#        response = bytes("{}: {}".format(cur_thread.name, data), 'ascii')
-#        self.request.sendall(response)
-
-#class ThreadedTCPServer(socketserver.ThreadingMixIn, socketserver.TCPServer):
-#    pass
-
-def client (ip, port, message, wait, rank, num_procs, s_time):
+def client (ip, port, message, wait, id, num_procs, s_time):
     # simulate computation
     time.sleep (wait)
-    print (str(time.time()-s_time-wait) +"\n"+ str(rank) + " "+ str(neighbors (rank, num_procs)))
+    print (str(time.time()-s_time-wait) +"\n"+ str(id) + " "+ str(neighbors (id, num_procs)))
     print ()
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
         sock.connect((ip, port))
@@ -29,93 +24,133 @@ def client (ip, port, message, wait, rank, num_procs, s_time):
         print("Received: {}".format(response))
         
         
-# returns int arr of all neighboring ranks
+# returns int arr of all neighboring ids
 # assumes # of procs is pow of 2
 # and neighbors are what they are in a log2(n)xlog2(n) array
 # diagonals are not neighbors
-def neighbors (rank, num_proc):
+def neighbors (id, num_proc):
     neighbors = []
     base = math.floor (math.log2 (num_proc))
-    if (rank % base != 0):
-        neighbors.append (rank-1)
-    if (rank % base != base-1):
-        neighbors.append (rank+1)
-    if (rank - base >= 0):
-        neighbors.append (rank - base)
-    if (rank + base < num_proc):
-        neighbors.append (rank + base)
+    if (id % base != 0):
+        neighbors.append (id-1)
+    if (id % base != base-1):
+        neighbors.append (id+1)
+    if (id - base >= 0):
+        neighbors.append (id - base)
+    if (id + base < num_proc):
+        neighbors.append (id + base)
     return neighbors
+     
+def init_threads (id, barrier):
+    server = socket.socket (socket.AF_INET, socket.SOCK_STREAM)
+    print ("id=" +str(id))
+    global sum
+    # binds servers in consective ports
+    server.bind ((host, port_seed + id))
+    #print ("server " + str(i) + " created")
+    server.listen (num_procs)
+    node_lock = threading.Lock()
+    server_node = Node (server, id, port_seed+id, num_procs, node_lock)
+    lock.acquire()
+    servers.append (server_node)
+    lock.release()
+    barrier.wait()
+    
+    for neighbor in neighbors (id, num_procs):
+        server_communicator = socket.socket (socket.AF_INET, socket.SOCK_STREAM)
+        lock.acquire()
+        servers[i].addCommunicator (server_communicator, id)
+        sum = sum + 1
+        lock.release()
+    return;
+
+
+# messanger loop
+def messanger_loop (node):
+
+    while True:
+        #acquires lock
+        node.getLock().acquire()
+        # checks if node has message to send
+        if (node.hasMessage):
+            for port in (node.getMessagePorts()):
+                node.sendMessage (node.getMessage(), port)
+            # all messages have been sent
+            node.finishedMessages()
+        node.getLock().release()
+        wait (.1)
+
+# ** main loop **
+def server_loop (node):
+    work_done = False
+    message = "" + str (node.getId())
+    while True:
+        # do work
+        # get work in byte stream to send
+        # get and set message in node
+        node.getLock().acquire()
+        if (work_done):
+            node.setMessage (message)
+            ports = []
+            for neighbor in neighbors (node.getId(), num_procs):
+                ports.append(node.getPort() + neighbor)
+        node.getLock().release()
+        # recieve messages
+ 
      
 if __name__ == "__main__":
     #### Setup ####
-    host = 'localhost'
-    port_seed = 5789
+
     servers = []
     
-    
+    num_procs = 0
     if (len(sys.argv) != 2):
         print ("invalid usage: lacking num_proccesses")
         print ("valid usage: python run_dgnca <num processes>")
         quit()
     try:
         num_procs = int(sys.argv[1])
-        print (num_proc)
+        print (num_procs)
     except ValueError:
         print ("invalid usage: please enter a processes count to a power of 2")
         print ("valid usage: python run_dgnca <num processes>")
         quit()
-
-    
-
-    for i in range (num_procs):
-        server = socket.socket (socket.AF_INET, socket.SOCK_STREAM)
-        # binds servers in consective ports
-        server.bind ((host, port_seed + i))
-        print ("server " + str(i) + " created")
-        server.listen (num_procs)
-        server_node = Node (server, i, port_seed+i, num_procs)
-        servers.append (server_node)
-        
-    # add communicator sockets to server nodes for 
-    # their neighbors
-    sum = 0
-    for rank in range (num_procs):
-        for neighbor in neighbors (rank, num_procs):
-            server_communicator = socket.socket (socket.AF_INET, socket.SOCK_STREAM)
-            server_communicator.connect ((host, port_seed + rank))
-            servers[i].addCommunicator (server_communicator, rank)
-            sum += 1
-    print (sum)
-            
-    #for i in range (num_procs):
-        
+       
+       
     # load model
     # load data
-
-    # set up server    
+       
+    barrier = threading.Barrier (num_procs)
     
-    # create client proccesses to recieve data 
-    # send rank as message
-#   
-    # use rank to choose data to use
-    # connect to neighbors using rank
+    
+    server_threads = []
+    # spawn servers
+    for i in range (num_procs):
+        t = threading.Thread (target=init_threads, args=(i, barrier))
+        server_threads.append(t)
+        t.start()
         
-    ### Main loop ###
- #   to_stop = False
-  #  while (to_stop != False):
-        #   Compute update
+    for t in server_threads:
+        t.join()
+    # now we have num_proc nodes
+    print("num connections=" + str(sum))
+    server_threads = []
+    messanger_threads = []
+    for nodes in servers:
+        m = threading.Thread (target=messanger_loop)
+        s = threading.Thread (target=server_loop)
+    
+    print ("starting main loops")
+    for i in range(len(server_threads)):
+        #server_threads[i].start()
+        #messanger_threads[i].start()
         
-        #   Check for stop signal
-  #      if (True):
-   #         toStop = false
-        #   Broadcast update using tcp
-            # server recieves update first -> then broadcasts
-            # OR each proc gets its own server
-            # and communicates strictly with neighbors
-        #   Check for stop signal
-        #   Retrieve update from tcp buffer
-        #   Update data buffer
-    #server.shutdown()
+    print ("ending program")
+    for i in range(len(server_threads)):
+        # join threads
+        #server_threads[i].join()
+        #messanger_threads[i].join()
+        #servers[i].getServer().close()
     pass
 
 
