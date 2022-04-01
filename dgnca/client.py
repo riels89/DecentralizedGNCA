93956314841cc2ee8f1b1c0116f05722a71c4c15
Riley White <white3rd@dukes.jmu.edu>
2022-04-01 10:36:20 -0400

    switching to a highlow connection strategy

--- dgnca/client.py
+++ dgnca/client.py
@@ -6,24 +6,82 @@ from node import Node
 
 import threading
 import time
+import copy
+import os
 
 host = 'localhost'
-port = 8081
+port = 8080
 
 def wait_for_port(id, host, port_to_conn, timeout=5.0):
     """
     https://gist.github.com/butla/2d9a4c0f35ea47b7452156c96a4e7b12
     """
-    start_time = time.perf_counter()
+    start_time = time.time()
     while True:
         try:
             conn = socket.create_connection((host, port_to_conn))
             return conn
         except OSError as e:
             time.sleep(0.1)
-            if time.perf_counter() - start_time >= timeout:
+            end_time = time.time()
+            if end_time - start_time>= timeout:
+                print(f"{id} {end_time - start_time}")
                 raise TimeoutError('{} Waited too long for the port {} on host {} to start accepting '
                                    'connections.'.format(id, port_to_conn, host)) from e
+
+def listen_for_neighbors(id, node_port, connection_dict, mutex): 
+    mutex.acquire()
+    it_dict = copy.deepcopy(connection_dict)
+    mutex.release()
+
+    num_to_recieve = 0
+    for neighbor_id in it_dict.keys():
+        if int(neighbor_id) > id:
+            num_to_recieve += 1
+
+    for i in range(num_to_recieve):
+        if(id == 1):
+            print(f"{id}: {i}")
+        conn = socket.create_server((host, node_port), backlog=len(connection_dict))
+        conn, addr = conn.accept()
+        if conn == 0:
+            print(f"Failed to create listening connection.")
+        neighbor_id = conn.recv(1024).decode("ascii")
+        print(f"{id} successfully got connection {neighbor_id}")
+
+        mutex.acquire()
+        if neighbor_id not in connection_dict:
+            print(f"{id} {neighbor_id} not in dict")
+            raise Exception(f"{id} listen {neighbor_id} not in dict")
+        connection_dict[neighbor_id] = conn
+        mutex.release()
+
+
+def connect_to_neighbors(id, neighbor_ports, connection_dict, mutex):
+    mutex.acquire()
+    it_dict = copy.deepcopy(connection_dict)
+    mutex.release()
+    for neighbor_id, conn in it_dict.items():
+        if int(neighbor_id) > id:
+            continue
+
+        conn = wait_for_port(id, host, neighbor_ports[neighbor_id])
+
+        if conn == 0:
+            print(f"Failed to create connection from {id} to {neighbor_id}")
+        else:
+            print(f"{id} successfully connected to {neighbor_id}")
+        try:
+            conn.send(str(id).encode())
+        except Exception as e:
+            raise Exception(f"{id}, failed to send") from e
+
+        mutex.acquire()
+        if neighbor_id not in connection_dict:
+            print(f"{id} {neighbor_id} not in dict")
+            raise Exception(f"{id} conn {neighbor_id} not in dict")
+        connection_dict[neighbor_id] = conn
+        mutex.release()
 
 
 
@@ -41,76 +99,44 @@ def createClient():
     id = int(data[-12:-9])
     neighbor_ports = json.loads(data[:-12])
     connection_dict = {neighbor_id: None for neighbor_id in neighbor_ports.keys()}
+    print(f"{id} {neighbor_ports}")
 
     if id == 1:
         print(f"{1} {connection_dict}")
 
-    while(None in list(connection_dict.values())):
-        print(f"{id} Entered while")
-        if is_connecting:
-            for neighbor_id, conn in connection_dict.items():
-                if conn is not None:
-                    continue
-
-                conn = wait_for_port(id, host, neighbor_ports[neighbor_id])
-
-                if conn == 0:
-                    print(f"Failed to create connection from {id} to {neighbor_id}")
-                else:
-                    print(f"{id} successfully connected to {neighbor_id}")
-                try:
-                    conn.send(str(id).encode())
-                except Exception as e:
-                    raise Exception(f"{id}, failed to send") from e
-                connection_dict[neighbor_id] = conn
-        else:
-            print(f"{id} Attempting listen")
-            for i in range(num_to_recieve):
-                conn = socket.create_server((host, node_port), backlog=len(connection_dict))
-                conn, addr = conn.accept()
-                if conn == 0:
-                    print(f"Failed to create listening connection.")
-                neighbor_id = conn.recv(1024).decode("ascii")
-                print(f"{id} successfully got connection {neighbor_id}")
-                connection_dict[neighbor_id] = conn
-
-        if None in list(connection_dict.values()):
-            sock.send(b"finished_round")
-            data = sock.recv(100000).decode('ascii')
-            print(f"{id} data: {data}")
-            num_to_recieve = int(data[-3:])
-            is_connecting = data[-4] == 'T'
+    mutex = threading.Lock()
+    listening_thread = threading.Thread (target = listen_for_neighbors, args=(id, node_port, connection_dict, mutex))
+    connecting_thread = threading.Thread (target = connect_to_neighbors, args=(id, neighbor_ports, connection_dict, mutex))
+    listening_thread.start()
+    connecting_thread.start()
+    listening_thread.join()
+    connecting_thread.join()
 
     sock.send(b"all_connected")
     print(f"{id} finished")
     
-    # while sendCount < 6:
-    #     message = f'Node: {myNode.getId()}, sent: {sendCount}'
-    #     sock.send(message.encode('ascii'))
-
-    #     data = sock.recv(1024)
-
-    #     print(f"Received from server: {str(data.decode('ascii'))}")
-    #     sendCount += 1
-    
     sock.close()
 
 if __name__ == "__main__":
-    num_procs = 256
+    # num_procs = 50
     
-    threads = []
-    for i in range(num_procs):
-        thread = threading.Thread (target = createClient)
-        threads.append(thread)
-        thread.start()
+    # threads = []
+    # for i in range(num_procs):
+    #     thread = threading.Thread (target = createClient)
+    #     # pid = os.fork()
+    #     # if pid == 0:
+    #     #     createClient()
+    #     threads.append(thread)
+    #     thread.start()
     
-    for thread in threads:
-        thread.join()
+    # for thread in threads:
+    #     thread.join()
     
-    print(f'Created: {len(threads)} clients')
+    # print(f'Created: {len(threads)} clients')
 
-    for thread in threads:
-        thread.join()
+    # for thread in threads:
+    #     thread.join()
     
-    print(f'Closed: {len(threads)} clients')
+    # print(f'Closed: {len(threads)} clients')
+    createClient()
 
--- dgnca/main_server.py
+++ dgnca/main_server.py
@@ -11,7 +11,7 @@ import json
 from dgnca import select_node
 
 
-port = 8081
+port = 8080
 host = 'localhost'
 print_lock = threading.Lock()
 
@@ -37,53 +37,8 @@ def threaded(client, id, port, neighbor_ports, node_order):
         client.close()
         return
     
-    received_all_connected = False
-    for i in range(1, len(node_order)):
-        if data == 'all_connected':
-            received_all_connected = True
-            break
-
-        in_node_order = "T" if id in node_order[i] else "F"
-        num_to_recieve = 0
-        if in_node_order == "F":
-            for neighbor_id in neighbor_ports.keys():
-                if neighbor_id in node_order[i]:
-                    num_to_recieve += 1
-
-        encoded_neighbors = str.encode(f"{in_node_order}{num_to_recieve:03d}")
-        client.send(encoded_neighbors)
-
-        data = str(client.recv(1024), 'ascii')
-        print(f"{id} recieved: {data}")
-        if data == "all_connected":
-            received_all_connected = True
-            break
-        elif data != "finished_round":
-            print(f"{id}: received something other than \'finished_round\'")
-            client.close()
-    
-    if not received_all_connected:
-        data = str(client.recv(1024), 'ascii')
-        print(f"{id} recieved: {data}")
-        if data != "all_connected":
-            print(f"{id}: received something other than \'all_connected\'")
-
     print("Finished")
 
-    # while True:
-    #     data = client.recv(1024)
-    #     if not data:
-    #         print('Connection terminated')
-
-    #         #print_lock.release()
-    #         break
-        
-    #     print(f'Server Received {data}')
-        
-    #     # Send back something to client
-    #     returnMessage = 'Recieved from client: ' + str(data.decode('ascii'))[-1] 
-    #     client.send( returnMessage.encode('ascii'))
-    
     client.close()
 
 
@@ -91,22 +46,11 @@ if __name__ == "__main__":
 
     #### Setup ####
     g = pygsp.graphs.Grid2d()
-    adj = np.squeeze(np.asarray(g.W.todense()))
-    # adj = np.array([[0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0],
-    #                 [1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0],
-    #                 [0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0],
-    #                 [0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0],
-    #                 [0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1],
-    #                 [1, 1, 0, 1, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0],
-    #                 [0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    #                 [0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0],
-    #                 [0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0],
-    #                 [1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
-    #                 [0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    #                 [0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0],
-    #                 [0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0],
-    #                 [0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0],
-    #                 [0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]])
+    adj = np.squeeze(np.asarray(g.W.todense()))[:50, :50]
+    # adj = np.array([[0, 1, 1, 0],
+    #                 [1, 0, 1, 1],
+    #                 [1, 1, 0, 1],
+    #                 [0, 1, 1, 0]])
     node_order = select_node(adj)
     print(f'node order {node_order}')
 
@@ -116,11 +60,12 @@ if __name__ == "__main__":
     neighbors = {i:[int(num) for num in np.where(adj[i])[0]] for i in ids}
 
     ports = []
-    for i in ids:
-        node_port = randint(49152,65535) 
-        while node_port in ports:
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        for i in ids:
             node_port = randint(49152,65535) 
-        ports.append(node_port)
+            while node_port in ports or s.connect_ex(('localhost', port)) == 0:
+                node_port = randint(49152,65535) 
+            ports.append(node_port)
 
     # Creating Main Server
     mainServer = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
